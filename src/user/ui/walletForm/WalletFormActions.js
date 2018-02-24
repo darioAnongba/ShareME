4d4244556f37dc28b3bd6bfa0f7c59bb4e3e8cd6
Dario Anongba Varela <dario.anongba@gmail.com>
2018-02-24 21:46:51 +0100

    Transfers done

--- src/user/ui/walletForm/WalletFormActions.js
+++ src/user/ui/walletForm/WalletFormActions.js
@@ -3,13 +3,13 @@ import store from '../../../store'
 
 const contract = require('truffle-contract');
 
-export function transferToUser(toAddress, amount) {
+export function transferToUser(toAddress, amount, dispatch) {
     let web3 = store.getState().web3.web3Instance;
 
     // Double-check web3's status.
     if (typeof web3 !== 'undefined') {
 
-        return function(dispatch) {
+        return function() {
             const token = contract(TokenContract);
             token.setProvider(web3.currentProvider);
 
@@ -28,9 +28,8 @@ export function transferToUser(toAddress, amount) {
                     return tokenInstance.transfer(toAddress, amount, {from: accounts[0]})
                 }).then(function (result) {
                     alert('Transfer successful !');
-                    return {
-                        type: 'TEST'
-                    }
+                    return getBalances(dispatch);
+
                 }).catch(function (err) {
                     console.log(err.message);
                 });
@@ -41,6 +40,43 @@ export function transferToUser(toAddress, amount) {
     }
 }
 
-export function getBalances() {
+export function getBalances(dispatch) {
+    console.log('Getting balances...');
 
+    let web3 = store.getState().web3.web3Instance;
+
+    // Double-check web3's status.
+    if (typeof web3 !== 'undefined') {
+
+        return function () {
+            const token = contract(TokenContract);
+            token.setProvider(web3.currentProvider);
+
+            // Declaring this for later so we can chain functions on Authentication.
+            let tokenInstance;
+
+            // Get current ethereum wallet.
+            web3.eth.getAccounts((error, accounts) => {
+                // Log errors, if any.
+                if (error) {
+                    console.error(error);
+                }
+
+                token.deployed().then(function(instance) {
+                    tokenInstance = instance;
+
+                    // Get balances
+                    return tokenInstance.balanceOf(accounts[0]);
+                }).then(function (result) {
+                    const balance = result.c[0];
+                    dispatch({type: 'GET_BALANCES_SUCCESS', payload: balance});
+
+                }).catch(function (err) {
+                    console.log(err.message);
+                });
+            })
+        }
+    } else {
+        console.error('Web3 is not initialized.');
+    }
 }
--- src/user/ui/walletForm/WalletFormContainer.js
+++ src/user/ui/walletForm/WalletFormContainer.js
@@ -1,9 +1,6 @@
 import { connect } from 'react-redux'
 import WalletForm from './WalletForm'
-import { transferToUser } from './WalletFormActions'
-import store from "../../../store";
-import TokenContract from '../../../../build/contracts/ShareMeToken.json'
-const contract = require('truffle-contract');
+import { transferToUser, getBalances } from './WalletFormActions'
 
 const mapStateToProps = (state, ownProps) => {
     return state.user
@@ -12,47 +9,10 @@ const mapStateToProps = (state, ownProps) => {
 const mapDispatchToProps = (dispatch) => {
     return {
         onTransferFormSubmit: (address, amount) => {
-          console.log(address, amount);
-            dispatch(transferToUser(address, amount))
+            dispatch(transferToUser(address, amount, dispatch))
         },
-        fetchBalances: () => {
-            console.log('Getting balances...');
-
-            let web3 = store.getState().web3.web3Instance;
-
-            // Double-check web3's status.
-            if (typeof web3 !== 'undefined') {
-                const token = contract(TokenContract);
-                token.setProvider(web3.currentProvider);
-
-                // Declaring this for later so we can chain functions on Authentication.
-                let tokenInstance;
-
-                // Get current ethereum wallet.
-                web3.eth.getAccounts((error, accounts) => {
-                    // Log errors, if any.
-                    if (error) {
-                        console.error(error);
-                    }
-
-                    token.deployed().then(function(instance) {
-                        tokenInstance = instance;
-
-                        // Get balances
-                        return tokenInstance.balanceOf(accounts[0]);
-                    }).then(function (result) {
-                        const balance = result.c[0];
-                        dispatch({type: 'GET_BALANCES_SUCCESS', payload: balance});
-
-                    }).catch(function (err) {
-                        console.log(err.message);
-                    });
-                })
-            } else {
-                console.error('Web3 is not initialized.');
-            }
-        }
-    };
+        fetchBalances: getBalances(dispatch)
+    }
 };
 
 const WalletFormContainer = connect(
