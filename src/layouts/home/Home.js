ccd4e503f8a0efa7bc4a5146d0bea756208fa1dd
Dario Anongba Varela <dario.anongba@gmail.com>
2018-02-25 08:33:30 +0100

    Hardcoded startimes

--- src/layouts/home/Home.js
+++ src/layouts/home/Home.js
@@ -1,27 +1,33 @@
 import React, { Component } from 'react'
 
 class Home extends Component {
-  render() {
-    return(
-      <main className="container">
-        <div className="pure-g">
-          <div className="pure-u-1-1">
-            <h1>Good to Go!</h1>
-            <p>Your Truffle Box is installed and ready.</p>
-            <h2>Smart Contract Authentication</h2>
-            <p>This particular box comes with autentication via a smart contract built-in.</p>
-            <p>In the upper-right corner, you'll see a login button. Click it to login with with the Authentication smart contract. If there is no user information for the given address, you'll be redirected to sign up. There are two authenticated routes: "/dashboard", which displays the user's name once authenticated; and "/profile", which allows a user to update their name.</p>
-            <h3>Redirect Path</h3>
-            <p>This example redirects home ("/") when trying to access an authenticated route without first authenticating. You can change this path in the failureRedriectUrl property of the UserIsAuthenticated wrapper on <strong>line 9</strong> of util/wrappers.js.</p>
-            <h3>Accessing User Data</h3>
-            <p>Once authenticated, any component can access the user's data by assigning the authData object to a component's props.<br/><code>{"// In component's render function."}<br/>{"const { authData } = this.props"}<br/><br/>{"// Use in component."}<br/>{"Hello { this.props.authData.name }!"}</code></p>
-            <h3>Further Reading</h3>
-            <p>The React/Redux portions of the authentication fuctionality are provided by <a href="https://github.com/mjrussell/redux-auth-wrapper" target="_blank">mjrussell/redux-auth-wrapper</a>.</p>
-          </div>
-        </div>
-      </main>
-    )
-  }
+    render() {
+        return(
+            <main className="container">
+                <div className="pure-g">
+                    <div className="pure-u-1-1">
+                        <h1>ShareME !</h1>
+                        <p>Decentralized Car sharing platform</p>
+                        <h2>Be your own Car rental !</h2>
+                        <p>
+                            In order to solve the global problem of cars not being used most of the time.
+                            The average car is <strong>used only 4% of the time in Switzerland!</strong>
+                        </p>
+                        <h2>Get rewards !</h2>
+                        <p>
+                            Put your car to rental and earn SMT tokens that can be used to rent cars on the system,
+                            get amazing rewards and can exchanged for other tokens.
+                        </p>
+
+                        <h2>Try it !</h2>
+                        <p>
+                            Rent a car without needing intermediaries that take immense transaction fees !
+                        </p>
+                    </div>
+                </div>
+            </main>
+        )
+    }
 }
 
 export default Home
