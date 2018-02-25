a6b4ef1462b44243d0f0cc344c3cbe04e9437f9d
Dario Anongba Varela <dario.anongba@gmail.com>
2018-02-25 08:52:42 +0100

    POST

--- src/user/ui/newCarForm/NewCarFormActions.js
+++ src/user/ui/newCarForm/NewCarFormActions.js
@@ -1,6 +1,6 @@
 import LoanContract from '../../../../build/contracts/Loan.json'
 import store from '../../../store'
-
+import request from "superagent";
 const contract = require('truffle-contract');
 
 export const CAR_ADDED = 'CAR_ADDED';
@@ -26,13 +26,31 @@ export function addCar(fields) {
 
                 loanContract.deployed().then(function(instance) {
                     loanContractInstance = instance;
-
                     return loanContractInstance.addCar(web3.fromUtf8(fields.plateNumber), {from: accounts[0]})
                 }).then(function (success) {
                     alert('Car added successfully !');
 
+                    // Do POST to server
+                    request
+                        .post('http://130.82.239.32:8000/shareme/car')
+                        .set('Content-Type', 'application/x-www-form-urlencoded')
+                        .send(
+                            {
+                                user_address: accounts[0],
+                                user_name: 'Dario',
+                                brand: fields.brand,
+                                plate_number: fields.plateNumber,
+                                nb_seats: fields.nbSeats,
+                                nb_kms: fields.nbKms,
+                                picture_url: fields.pictureURL
+                            }
+                        )
+                        .end(function(err, res){
+                            console.log(res.text);
+                        });
+
                     return {
-                      type: CAR_ADDED
+                        type: CAR_ADDED
                     }
 
                 }).catch(function (err) {
