3f9b74ec84fc7c52da390bdd986635ab413d9c4a
Dario Anongba Varela <dario.anongba@gmail.com>
2018-02-25 02:12:55 +0100

    Add car

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,15 @@
-var Ownable = artifacts.require("./zeppelin/ownership/Ownable.sol");
-var Killable = artifacts.require("./zeppelin/lifecycle/Killable.sol");
-var Authentication = artifacts.require("./Authentication.sol");
-var ShareMeToken = artifacts.require("ShareMeToken.sol");
+let Ownable = artifacts.require("./zeppelin/ownership/Ownable.sol");
+let Killable = artifacts.require("./zeppelin/lifecycle/Killable.sol");
+let Authentication = artifacts.require("./Authentication.sol");
+let ShareMeToken = artifacts.require("./ShareMeToken.sol");
+let Loan = artifacts.require("./Loan.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Ownable);
-  deployer.link(Ownable, Killable);
-  deployer.deploy(Killable);
-  deployer.link(Killable, Authentication);
-  deployer.deploy(Authentication);
-  deployer.deploy(ShareMeToken);
+    deployer.deploy(Ownable);
+    deployer.link(Ownable, Killable);
+    deployer.deploy(Killable);
+    deployer.link(Killable, Authentication);
+    deployer.deploy(Authentication);
+    deployer.deploy(ShareMeToken);
+    deployer.deploy(Loan);
 };
